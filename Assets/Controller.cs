68ddc2b3ce105be04485a7f1fb50888b23595b85
schmelto <tom.schmelzer@web.de>
2021-04-08 12:58:11 +0200

    first commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    private float moveInput;
+    private float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // physics every time in FixedUpdate
+    void FixedUpdate()
+    {
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y);
+        
+    }
+}
+
+
